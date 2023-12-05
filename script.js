d1456ee70128140bfdd467bc0b61f9791a54baba
Francis Plante <francis.plante@libeo.com>
2023-12-05 13:30:21 -0500

    scroll and click links logic

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
+rightPanelElement = document.getElementById("right");
+gradientColor = "rgba(56,56,251,0.4)";
 function setGradient(e) {
   // Get the viewport width and height
   var vw = window.innerWidth;
   var vh = window.innerHeight;
 
-  rightPanel = document.querySelector(".right");
-  console.log(rightPanel.scrollTop);
+  var percentX = (e.clientX / vw) * 100;
+  var percentY = (e.clientY / vh) * 100;
 
-  rightPanel.addEventListener("scroll", function () {
-    console.log(rightPanel.scrollTop);
-    var percentX = (e.clientX / vw) * 20 + 130;
-    var percentY = (e.clientY / vh) * rightPanel.scrollTop * 0.6;
+  gradientElement = document.getElementById("gradient");
 
-    // Set the gradient as the body background
-    gradientEl = document.getElementById("gradient");
-    gradientEl.style.background = `radial-gradient(circle at ${percentX}% ${percentY}%,rgba(0,170,255,0.3) 0%, rgba(0,0,0,0.3) 60%)`;
+  gradientElement.style.background = `radial-gradient(circle at ${percentX}% ${percentY}%, ${gradientColor} 0%, rgba(6,0,34,0.4) 40%)`;
+}
+
+document.addEventListener("mousemove", setGradient);
+
+// Function to set the active class based on the target element's visibility
+function setActiveClass() {
+  var links = document.querySelectorAll(".link");
+
+  links.forEach(function (link) {
+    var targetId = link.getAttribute("href").substring(1);
+
+    var targetElement = document.getElementById(targetId);
+
+    // Get the target element's position
+    var targetRect = targetElement.getBoundingClientRect();
+
+    // Check if the target element is in view
+
+    console.log(
+      `target.top ${targetRect.top} \n targetRect.bottom ${targetRect.bottom} window.innerHeigh ${window.innerHeight} clientHeight ${document.documentElement.clientHeight}`
+    );
+    if (targetRect.top >= 0 && targetRect.top <= 300) {
+      links.forEach(function (link) {
+        link.classList.remove("active");
+      });
+
+      // Set the active class on the link associated with the in-view target element
+      link.classList.add("active");
+    }
   });
 }
 
-// Add event listener to call the function on mousemove
-document.addEventListener("mousemove", setGradient);
+// Function to handle link clicks
+function handleLinkClick(event) {
+  var clickedLink = event.target;
+
+  // Add the "active" class to the clicked link
+  links.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  clickedLink.classList.add("active");
+}
+
+// Attach event listener to the scroll event
+
+rightPanelElement.addEventListener("scroll", setActiveClass);
+
+// Attach click event listeners to each link
+var links = document.querySelectorAll(".link");
+links.forEach(function (link) {
+  link.addEventListener("click", handleLinkClick);
+});
+
+// Initial call to set active class on page load
+setActiveClass();
